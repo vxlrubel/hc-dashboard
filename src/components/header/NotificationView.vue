a25db593bc421e840a0503f053390f995a98ed9f
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-12 02:09:12 +0600

    add avatar components and update notification view

--- src/components/header/NotificationView.vue
+++ src/components/header/NotificationView.vue
@@ -9,6 +9,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { CalendarDaysIcon } from '@lucide/vue'
 </script>
 
 <template>
@@ -19,12 +22,34 @@ import {
       </Button>
     </DropdownMenuTrigger>
     <DropdownMenuContent class="w-75">
-      <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuLabel>
+        <div class="flex items-center justify-between">
+          <span>Notifications (3)</span>
+          <Button variant="link" class="cursor-pointer">View All</Button>
+        </div>
+        <div class="text-sm text-muted-foreground">You have 3 unread notifications</div>
+      </DropdownMenuLabel>
       <DropdownMenuSeparator />
-      <DropdownMenuItem>Profile</DropdownMenuItem>
-      <DropdownMenuItem>Billing</DropdownMenuItem>
-      <DropdownMenuItem>Team</DropdownMenuItem>
-      <DropdownMenuItem>Subscription</DropdownMenuItem>
+      <DropdownMenuItem class="flex space-x-2 cursor-pointer" v-for="n in 20" :key="n">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div class="space-y-1 flex-1">
+          <div class="line-clamp-1">Complete Activity</div>
+          <div class="text-xs text-muted-foreground line-clamp-1">
+            You have completed the activity.
+          </div>
+          <div class="flex items-center pt-1">
+            <CalendarDaysIcon class="mr-1 size-3 opacity-70" />
+            <span class="text-[10px] text-muted-foreground pt-0.5">5 Jun 2025 : 10:00 AM</span>
+          </div>
+        </div>
+      </DropdownMenuItem>
+      <DropdownMenuSeparator />
+      <DropdownMenuLabel class="text-center">
+        <Button variant="link" class="cursor-pointer">View All</Button>
+      </DropdownMenuLabel>
     </DropdownMenuContent>
   </DropdownMenu>
 </template>
