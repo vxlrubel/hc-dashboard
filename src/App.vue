4dec788a477ef351f39d8abd663872d2cddc2f56
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-12 02:59:17 +0600

    update sidebar menu button label to 'HC-Dashboard'

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@ import NavigationMenu from './components/header/NavigationMenu.vue'
                 <GalleryVerticalEnd class="size-4" />
               </div>
               <div class="grid flex-1 text-left text-sm leading-tight">
-                <span class="truncate font-semibold">Acme Inc</span>
+                <span class="truncate font-semibold">HC-Dashboard</span>
                 <span class="truncate text-xs">Enterprise</span>
               </div>
             </SidebarMenuButton>
