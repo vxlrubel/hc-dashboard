f52887afef811f785351de2d6367cd8e23f95ca4
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 04:21:51 +0600

    feat(forms): add form edit view with page title

---
+++ src/views/forms/EditView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Edit Form" subtitle="Update form information." />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
+      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
+      aperiam, voluptatibus mollitia!
+    </p>
+  </div>
+</template>
