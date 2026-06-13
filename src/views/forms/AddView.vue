be8bce59b59c7c040f4f67637dad7318fd4890e4
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 04:21:48 +0600

    feat(forms): add form creation view with page title

---
+++ src/views/forms/AddView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Add Form" subtitle="Create a new form." />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
+      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
+      aperiam, voluptatibus mollitia!
+    </p>
+  </div>
+</template>
