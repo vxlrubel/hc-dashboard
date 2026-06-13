9cd0b19867d5bdad63523d91c9f15534192bf491
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 04:26:16 +0600

    feat(activities): add activity creation view with page title

---
+++ src/views/activities/AddView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Add Activity" subtitle="Create a new activity." />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
+      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
+      aperiam, voluptatibus mollitia!
+    </p>
+  </div>
+</template>
