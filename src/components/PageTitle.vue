935bf6b91367f3cfe26c9fee9250edc58d67aeb1
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 03:59:17 +0600

    feat(ui): add separator to PageTitle component

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Separator from '@/components/ui/separator/Separator.vue'
 defineProps<{
   title: string
   subtitle?: string
@@ -6,12 +7,15 @@ defineProps<{
 </script>
 
 <template>
-  <div class="pt-3 pb-4 space-y-1">
-    <h1 class="text-[22px] font-medium" v-if="title">
-      {{ title }}
-    </h1>
-    <p class="text-muted-foreground text-sm" v-if="subtitle">
-      {{ subtitle }}
-    </p>
+  <div>
+    <div class="pt-3 pb-4 space-y-1">
+      <h1 class="text-[22px] font-medium" v-if="title">
+        {{ title }}
+      </h1>
+      <p class="text-muted-foreground text-sm" v-if="subtitle">
+        {{ subtitle }}
+      </p>
+    </div>
+    <Separator class="mb-4" />
   </div>
 </template>
