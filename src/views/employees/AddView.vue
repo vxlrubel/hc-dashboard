0e268af758d09ae61959370f9d6bccf0e97cf2cf
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 03:58:48 +0600

    feat(employees): add employee creation form view

---
+++ src/views/employees/AddView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Add Employee" subtitle="Create a new employee record." />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
+      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
+      aperiam, voluptatibus mollitia!
+    </p>
+  </div>
+</template>
