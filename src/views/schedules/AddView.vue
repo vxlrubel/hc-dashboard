aa37e6dceca8fa760cac84d6d2bef5648be3ba92
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 04:32:08 +0600

    feat(schedules): add schedule creation view with page title

---
+++ src/views/schedules/AddView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Add Schedule" subtitle="Create a new schedule." />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
+      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
+      aperiam, voluptatibus mollitia!
+    </p>
+  </div>
+</template>
