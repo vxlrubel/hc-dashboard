400e42fe6c6ec16a670141ea9999ae7f9987009a
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 03:59:22 +0600

    refactor(docs): remove duplicate separator in favor of PageTitle

--- src/views/DocsView.vue
+++ src/views/DocsView.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
-import Separator from '../components/ui/separator/Separator.vue'
 </script>
 
 <template>
   <div>
     <PageTitle title="Documentation" subtitle="Learn more about our platform and how to use it." />
-    <Separator class="mb-4" />
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
