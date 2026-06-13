86bdd7d82c3d27913973f7c80b8affeae4dcfbe6
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-14 02:29:27 +0600

    refactor AppSidebar to enhance layout with tooltips and collapsible sections for better navigation

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -16,9 +16,30 @@ import {
   SidebarMenuSubItem,
 } from '@/components/ui/sidebar'
 
-import { GalleryVerticalEnd, UserCog, ChevronRight, Users } from '@lucide/vue'
+import {
+  GalleryVerticalEnd,
+  LayoutDashboard,
+  ChevronRight,
+  Users,
+  Info,
+  User,
+  CreditCard,
+  Settings,
+  LogOut,
+} from '@lucide/vue'
+
 import { Collapsible, CollapsibleTrigger, CollapsibleContent } from '@/components/ui/collapsible'
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 </script>
 
 <template>
@@ -40,39 +61,27 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip
         </SidebarMenuItem>
       </SidebarMenu>
     </SidebarHeader>
+
     <SidebarContent>
       <SidebarGroup>
         <SidebarGroupLabel>General</SidebarGroupLabel>
         <SidebarGroupContent>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton as-child>
+              <SidebarMenuButton as-child tooltip="Dashboard">
                 <RouterLink to="/">
-                  <UserCog />
-                  <span>Dashboard </span>
+                  <LayoutDashboard />
+                  <span>Dashboard</span>
                 </RouterLink>
               </SidebarMenuButton>
             </SidebarMenuItem>
             <SidebarMenuItem>
-              <SidebarMenuButton as-child>
-                <RouterLink to="/about" class="block px-4 py-1.5 hover:bg-gray-700"
-                  >About Us</RouterLink
-                >
+              <SidebarMenuButton as-child tooltip="About Us">
+                <RouterLink to="/about">
+                  <Info />
+                  <span>About Us</span>
+                </RouterLink>
               </SidebarMenuButton>
-            </SidebarMenuItem>
-            <SidebarMenuItem>
-              <Tooltip>
-                <TooltipTrigger as-child>
-                  <SidebarMenuButton as-child>
-                    <RouterLink to="/about">
-                      <UserCog />
-                      <span>About Us</span>
-                    </RouterLink>
-                  </SidebarMenuButton>
-                </TooltipTrigger>
-
-                <TooltipContent side="right"> About Us </TooltipContent>
-              </Tooltip>
             </SidebarMenuItem>
           </SidebarMenu>
         </SidebarGroupContent>
@@ -80,76 +89,93 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip
 
       <SidebarGroup>
         <SidebarGroupLabel>Settings</SidebarGroupLabel>
-
-        <SidebarMenuItem>
-          <Collapsible>
-            <CollapsibleTrigger as-child>
-              <SidebarMenuButton class="group">
-                <Users />
-                <span>Users</span>
-                <ChevronRight
-                  class="ml-auto transition-transform duration-300 ease-in-out group-data-[state=open]:rotate-90"
-                />
-              </SidebarMenuButton>
-            </CollapsibleTrigger>
-            <CollapsibleContent>
-              <SidebarMenuSub>
-                <SidebarMenuSubItem>
-                  <SidebarMenuButton>
-                    <span>All Users</span>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <Collapsible>
+                <CollapsibleTrigger as-child>
+                  <SidebarMenuButton class="group" tooltip="Users">
+                    <Users />
+                    <span>Users</span>
+                    <ChevronRight
+                      class="ml-auto transition-transform duration-300 ease-in-out group-data-[state=open]:rotate-90"
+                    />
                   </SidebarMenuButton>
-                </SidebarMenuSubItem>
-
-                <SidebarMenuSubItem>
-                  <SidebarMenuButton>
-                    <span>Add User</span>
-                  </SidebarMenuButton>
-                </SidebarMenuSubItem>
-
-                <SidebarMenuSubItem>
-                  <SidebarMenuButton>
-                    <span>User Roles</span>
-                  </SidebarMenuButton>
-                </SidebarMenuSubItem>
-              </SidebarMenuSub>
-            </CollapsibleContent>
-          </Collapsible>
-          <Collapsible>
-            <CollapsibleTrigger as-child>
-              <SidebarMenuButton class="group">
-                <Users />
-                <span>Users</span>
-                <ChevronRight
-                  class="ml-auto transition-transform duration-200 ease-in-out group-data-[state=open]:rotate-90"
-                />
-              </SidebarMenuButton>
-            </CollapsibleTrigger>
-            <CollapsibleContent>
-              <SidebarMenuSub>
-                <SidebarMenuSubItem>
-                  <SidebarMenuButton>
-                    <span>All Users</span>
-                  </SidebarMenuButton>
-                </SidebarMenuSubItem>
-
-                <SidebarMenuSubItem>
-                  <SidebarMenuButton>
-                    <span>Add User</span>
-                  </SidebarMenuButton>
-                </SidebarMenuSubItem>
-
-                <SidebarMenuSubItem>
-                  <SidebarMenuButton>
-                    <span>User Roles</span>
-                  </SidebarMenuButton>
-                </SidebarMenuSubItem>
-              </SidebarMenuSub>
-            </CollapsibleContent>
-          </Collapsible>
-        </SidebarMenuItem>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    <SidebarMenuSubItem>
+                      <SidebarMenuButton>
+                        <span>All Users</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuSubItem>
+                    <SidebarMenuSubItem>
+                      <SidebarMenuButton>
+                        <span>Add User</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuSubItem>
+                    <SidebarMenuSubItem>
+                      <SidebarMenuButton>
+                        <span>User Roles</span>
+                      </SidebarMenuButton>
+                    </SidebarMenuSubItem>
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </Collapsible>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
       </SidebarGroup>
     </SidebarContent>
-    <SidebarFooter />
+
+    <SidebarFooter>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+              <SidebarMenuButton size="lg" tooltip="Account">
+                <Avatar class="size-8 rounded-lg">
+                  <AvatarFallback class="rounded-lg">HC</AvatarFallback>
+                </Avatar>
+                <div class="grid flex-1 text-left text-sm leading-tight">
+                  <span class="truncate font-semibold">Admin</span>
+                  <span class="truncate text-xs">admin@hc.com</span>
+                </div>
+              </SidebarMenuButton>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent side="right" class="w-56" align="start">
+              <DropdownMenuLabel class="font-normal">
+                <div class="flex flex-col gap-1">
+                  <p class="font-medium text-sm">Admin</p>
+                  <p class="text-muted-foreground text-xs">admin@hc.com</p>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <User class="mr-2" />
+                  Profile
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <CreditCard class="mr-2" />
+                  Billing
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Settings class="mr-2" />
+                  Settings
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <LogOut class="mr-2" />
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarFooter>
+
     <SidebarRail />
   </Sidebar>
 </template>
