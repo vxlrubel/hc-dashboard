f6cdc6394feadf1d689d650015e50c55e1518367
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:07 +0600

    feat(forms): add RouterLink to ListView for navigation to Add Form page

--- src/views/forms/ListView.vue
+++ src/views/forms/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Form List" subtitle="View and manage all forms." />
+    <PageTitle title="Form List" subtitle="View and manage all forms.">
+      <RouterLink to="/dashboard/form/add" class="button-primary-outline"
+        >Add New Form</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
