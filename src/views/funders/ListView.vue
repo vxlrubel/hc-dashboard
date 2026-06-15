5e0d5ea29d7fe8ecd2f28ce4811518c5dc8c586e
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:03 +0600

    feat(funders): add RouterLink to ListView for navigation to Add Funder page

--- src/views/funders/ListView.vue
+++ src/views/funders/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Funder List" subtitle="View and manage all funders." />
+    <PageTitle title="Funder List" subtitle="View and manage all funders.">
+      <RouterLink to="/dashboard/funder/add" class="button-primary-outline"
+        >Add New Funder</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
