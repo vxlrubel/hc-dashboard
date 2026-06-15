0c26468dd5e53bcb217bb6dc882180c708189793
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:13 +0600

    feat(invoices): add RouterLink to ListView for navigation to Add Invoice page

--- src/views/invoices/ListView.vue
+++ src/views/invoices/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Invoice List" subtitle="View and manage all invoices." />
+    <PageTitle title="Invoice List" subtitle="View and manage all invoices.">
+      <RouterLink to="/dashboard/invoice/add" class="button-primary-outline"
+        >Add New Invoice</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
