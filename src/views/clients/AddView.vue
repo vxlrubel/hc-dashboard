f98a472ab7432bd6d762661ccb58a43872afb212
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:02 +0600

    feat(clients): add RouterLink to AddView for navigation back to Client List

--- src/views/clients/AddView.vue
+++ src/views/clients/AddView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Add Client" subtitle="Create a new client record." />
+    <PageTitle title="Add Client" subtitle="Create a new client record.">
+      <RouterLink to="/dashboard/clients" class="button-primary-outline"
+        >All Clients</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
