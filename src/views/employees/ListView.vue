1b49319f6c7843d25ae820470a02b5147b1d079a
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:03:57 +0600

    feat(employees): add RouterLink to ListView for navigation to Add Employee page

--- src/views/employees/ListView.vue
+++ src/views/employees/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Employee List" subtitle="View and manage all employees." />
+    <PageTitle title="Employee List" subtitle="View and manage all employees.">
+      <RouterLink to="/dashboard/employee/add" class="button-primary-outline"
+        >Add New Employee</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
