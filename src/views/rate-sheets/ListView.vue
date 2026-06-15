4fb3b6741182190542f1af8be46dd68e9b14ec3b
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:14 +0600

    feat(rate-sheets): add RouterLink to ListView for navigation to Add Rate Sheet page

--- src/views/rate-sheets/ListView.vue
+++ src/views/rate-sheets/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Rate Sheet List" subtitle="View and manage all rate sheets." />
+    <PageTitle title="Rate Sheet List" subtitle="View and manage all rate sheets.">
+      <RouterLink to="/dashboard/rate-sheet/add" class="button-primary-outline"
+        >Add New Rate Sheet</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
