6deef665821f5a0f00ab19a603501ddbe3c1ef3a
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-15 22:24:28 +0600

    feat(rate-sheets): add rate sheet detail view with page title

---
+++ src/views/rate-sheets/DetailView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Rate Sheet Details" subtitle="View and manage rate sheet information." />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
+      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
+      aperiam, voluptatibus mollitia!
+    </p>
+  </div>
+</template>
