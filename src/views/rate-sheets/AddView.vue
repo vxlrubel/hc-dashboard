f2dbfb37f3c9569a95c9799a74e33bd71bf09e6f
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:14 +0600

    feat(rate-sheets): add RouterLink to AddView for navigation back to Rate Sheet List

--- src/views/rate-sheets/AddView.vue
+++ src/views/rate-sheets/AddView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Add Rate Sheet" subtitle="Create a new rate sheet." />
+    <PageTitle title="Add Rate Sheet" subtitle="Create a new rate sheet.">
+      <RouterLink to="/dashboard/rate-sheets" class="button-primary-outline"
+        >All Rate Sheets</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
