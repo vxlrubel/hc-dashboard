aa401ac249ddd4bedecbe9e4bd61e6a71db31a2a
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:02 +0600

    feat(clients): add RouterLink to ListView for navigation to Add Client page

--- src/views/clients/ListView.vue
+++ src/views/clients/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Client List" subtitle="View and manage all clients." />
+    <PageTitle title="Client List" subtitle="View and manage all clients.">
+      <RouterLink to="/dashboard/client/add" class="button-primary-outline"
+        >Add New Client</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
