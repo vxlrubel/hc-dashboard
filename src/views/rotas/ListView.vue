270ea387b42067e3b2abfdd3cedbf88206cdd780
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:08 +0600

    feat(rotas): add RouterLink to ListView for navigation to Add Rota page

--- src/views/rotas/ListView.vue
+++ src/views/rotas/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Rota List" subtitle="View and manage all rotas." />
+    <PageTitle title="Rota List" subtitle="View and manage all rotas.">
+      <RouterLink to="/dashboard/rota/add" class="button-primary-outline"
+        >Add New Rota</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
