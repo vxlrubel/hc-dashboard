b6f8d9233e35d136a873ab21f4cef8d991a1793d
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-15 23:44:43 +0600

    feat(layouts): add DashboardLayout with sidebar, nav, and sidebar inset

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
+import NavigationView from '@/components/header/NavigationView.vue'
+import AppSidebar from '@/components/AppSidebar.vue'
+import { TooltipProvider } from '@/components/ui/tooltip'
+</script>
+
+<template>
+  <SidebarProvider>
+    <TooltipProvider>
+      <AppSidebar />
+    </TooltipProvider>
+    <SidebarInset>
+      <NavigationView />
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <RouterView />
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
+</template>
