83618f27921026c8ac3f887d978fac497be1c346
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:14 +0600

    feat(leave): add RouterLink to ListView for navigation to Add Leave page

--- src/views/leave/ListView.vue
+++ src/views/leave/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Leave List" subtitle="View and manage all leave requests." />
+    <PageTitle title="Leave List" subtitle="View and manage all leave requests.">
+      <RouterLink to="/dashboard/leave/add" class="button-primary-outline"
+        >Add New Leave</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
