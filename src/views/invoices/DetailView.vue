ce7bde51fdd8668c6e108211119ddc2e9fee51a3
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-15 22:21:42 +0600

    feat(invoices): add invoice detail view with page title

---
+++ src/views/invoices/DetailView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import PageTitle from '@/components/PageTitle.vue'
+</script>
+
+<template>
+  <div>
+    <PageTitle title="Invoice Details" subtitle="View and manage invoice information." />
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
+      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
+      aperiam, voluptatibus mollitia!
+    </p>
+  </div>
+</template>
