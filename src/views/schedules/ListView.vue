4b6a206c29c8a57f7a8e6940187312462dc25e85
Rubel Mahmud <vxlrubel@gmail.com>
2026-06-16 02:04:09 +0600

    feat(schedules): add RouterLink to ListView for navigation to Add Schedule page

--- src/views/schedules/ListView.vue
+++ src/views/schedules/ListView.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
 import PageTitle from '@/components/PageTitle.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <div>
-    <PageTitle title="Schedule List" subtitle="View and manage all schedules." />
+    <PageTitle title="Schedule List" subtitle="View and manage all schedules.">
+      <RouterLink to="/dashboard/schedule/add" class="button-primary-outline"
+        >Add New Schedule</RouterLink
+      >
+    </PageTitle>
     <p>
       Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
       Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
