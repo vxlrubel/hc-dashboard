94782663df7f2fdbbef5ab0c1637b38ff7d0a7f4
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-06 03:00:56 +0600

    feat: add clients action dropdown component with edit, view, delete, and assign options

---
+++ src/views/clients/ClientAction.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { Ellipsis, PencilSparkles, Trash2, UserRoundArrowLeft, Eye } from '@lucide/vue'
+
+import ActionText from '@/constants/action-text'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { useRouter } from 'vue-router'
+import { useClientsStore } from '@/stores/client'
+import { useConfirmDialog } from '@/composables/useConfirmDialog'
+
+defineProps<{
+  id: number | string
+}>()
+
+const router = useRouter()
+const store = useClientsStore()
+const { confirm } = useConfirmDialog()
+
+const handEditAction = (id) => {
+  const url = `/dashboard/client/edit/${id}`
+  router.push(url)
+}
+
+const handDeleteAction = async (id) => {
+  const confirmed = await confirm('Are you sure you want to move this client to trash?', {
+    confirmLabel: 'Yes, move',
+    cancelLabel: 'No',
+  })
+  if (confirmed) {
+    await store.deleteClient(id)
+  }
+}
+
+const handViewAction = (id) => {
+  const url = `/dashboard/client/show/${id}`
+  router.push(url)
+}
+
+const handAsignAction = (id) => {
+  alert('Asign id is : ' + id)
+}
+</script>
+
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger class="w-full flex justify-center">
+      <Ellipsis />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent class="w-45 rounded">
+      <DropdownMenuLabel class="text-xs opacity-75">Client Action</DropdownMenuLabel>
+
+      <DropdownMenuSeparator />
+
+      <DropdownMenuItem class="cursor-pointer" @click.prevent="handEditAction(id)">
+        {{ ActionText.edit }}
+        <DropdownMenuShortcut>
+          <PencilSparkles />
+        </DropdownMenuShortcut>
+      </DropdownMenuItem>
+      <DropdownMenuItem class="cursor-pointer" @click.prevent="handViewAction(id)">
+        {{ ActionText.view }}
+        <DropdownMenuShortcut>
+          <Eye />
+        </DropdownMenuShortcut>
+      </DropdownMenuItem>
+      <DropdownMenuItem class="cursor-pointer" @click.prevent="handDeleteAction(id)">
+        {{ ActionText.trash }}
+        <DropdownMenuShortcut>
+          <Trash2 />
+        </DropdownMenuShortcut>
+      </DropdownMenuItem>
+      <DropdownMenuItem class="cursor-pointer" @click.prevent="handAsignAction(id)">
+        {{ ActionText.assign }}
+        <DropdownMenuShortcut>
+          <UserRoundArrowLeft />
+        </DropdownMenuShortcut>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
