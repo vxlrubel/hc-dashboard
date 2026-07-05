88b159e9704a8cd4007e189be3788e1cfc7239de
Rubel Mahmud <vxlrubel@gmail.com>
2026-07-06 03:01:56 +0600

    feat: implement rate sheets list view with table, pagination, bulk actions, and date filtering

--- src/views/rate-sheets/ListView.vue
+++ src/views/rate-sheets/ListView.vue
@@ -1,19 +1,265 @@
 <script setup lang="ts">
+import { watch, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useRoute, useRouter } from 'vue-router'
 import PageTitle from '@/components/PageTitle.vue'
 import { RouterLink } from 'vue-router'
+import { Table, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectGroup,
+  SelectLabel,
+  SelectValue,
+} from '@/components/ui/select'
+
+import RateSheetAction from './RateSheetAction.vue'
+
+import type { DateValue } from '@internationalized/date'
+import { DateFormatter, getLocalTimeZone, today } from '@internationalized/date'
+import { CalendarIcon, X, Loader } from '@lucide/vue'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+
+import ActivityPagination from '@/views/activities/ActivityPagination.vue'
+
+import { Checkbox } from '@/components/ui/checkbox'
+import { Label } from '@/components/ui/label'
+import { ukFormat } from '@/utils/dateFormat'
+import { getStatus } from '@/utils/status'
+import { useRateSheetsStore } from '@/stores/rateSheet'
+const rateSheetStore = useRateSheetsStore()
+
+const route = useRoute()
+const router = useRouter()
+
+const pageParam = route.query.page
+if (pageParam) {
+  rateSheetStore.page = Number(pageParam)
+} else {
+  rateSheetStore.page = 1
+}
+
+const defaultPlaceholder = today(getLocalTimeZone())
+const date = ref() as Ref<DateValue>
+const toDate = ref() as Ref<DateValue>
+const df = new DateFormatter('en-GB', {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric',
+})
+
+watch(
+  () => rateSheetStore.page,
+  (newPage) => {
+    if (newPage > 1) {
+      router.replace({ query: { page: String(newPage) } })
+    } else {
+      router.replace({ query: {} })
+    }
+  },
+)
+
+const { paginatedRateSheets, page, itemsPerPage, totalRateSheets, buikLoading, deletingIds } =
+  storeToRefs(rateSheetStore)
+
+async function onApply() {
+  await rateSheetStore.handleBuilAction()
+}
 </script>
 
 <template>
   <div>
     <PageTitle title="Rate Sheet List" subtitle="View and manage all rate sheets.">
-      <RouterLink to="/dashboard/rate-sheet/add" class="button-primary-outline"
-        >Add New Rate Sheet</RouterLink
-      >
+      <RouterLink to="/dashboard/rate-sheet/add">
+        <Button class="button-primary-outline">Add New Rate Sheet</Button>
+      </RouterLink>
     </PageTitle>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod perferendis iusto officia!
-      Repudiandae necessitatibus animi, culpa delectus accusamus quis cum voluptatem dolorem,
-      aperiam, voluptatibus mollitia!
-    </p>
+
+    <div class="flex flex-col items-start md:flex-row md:items-center gap-4 mb-2">
+      <div class="flex items-center gap-2 w-full md:w-62">
+        <div class="flex-1">
+          <Select>
+            <SelectTrigger class="w-full">
+              <SelectValue placeholder="Bulk action" />
+            </SelectTrigger>
+            <SelectContent class="w-full">
+              <SelectGroup>
+                <SelectLabel>Actions</SelectLabel>
+                <SelectItem value="edit"> Edit </SelectItem>
+                <SelectItem value="trash"> Move to trash </SelectItem>
+                <SelectItem value="delete"> Delete </SelectItem>
+                <SelectItem value="restore"> Restore </SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </div>
+        <Button :disabled="buikLoading" variant="outline" @click.prevent="onApply" class="w-18.75">
+          <template v-if="buikLoading">
+            <Loader class="animate-spin" />
+          </template>
+          <template v-else>Apply</template>
+        </Button>
+      </div>
+      <div class="flex justify-between flex-wrap w-full md:w-100 md:flex-nowrap gap-2">
+        <div class="w-[48%]">
+          <Popover v-slot="{ close }">
+            <PopoverTrigger as-child>
+              <Button
+                variant="outline"
+                :class="
+                  cn(
+                    'w-full justify-start text-left font-normal rounded relative',
+                    !date && 'text-muted-foreground',
+                  )
+                "
+              >
+                <CalendarIcon />
+                {{ date ? df.format(date.toDate(getLocalTimeZone())) : 'Start date' }}
+
+                <span
+                  v-if="date"
+                  class="absolute top-0 bottom-0 right-0 inline-flex items-center px-2"
+                  @click.stop="date = undefined"
+                >
+                  <X />
+                </span>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent class="w-auto p-0" align="start">
+              <Calendar
+                v-model="date"
+                :default-placeholder="defaultPlaceholder"
+                layout="month-and-year"
+                initial-focus
+                @update:model-value="close"
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        <div class="w-[48%]">
+          <Popover v-slot="{ close }">
+            <PopoverTrigger as-child>
+              <Button
+                variant="outline"
+                :class="
+                  cn(
+                    'w-full justify-start text-left font-normal rounded relative',
+                    !toDate && 'text-muted-foreground',
+                  )
+                "
+              >
+                <CalendarIcon />
+                {{ toDate ? df.format(toDate.toDate(getLocalTimeZone())) : 'End date' }}
+                <span
+                  v-if="toDate"
+                  class="absolute top-0 bottom-0 right-0 inline-flex items-center px-2"
+                  @click.stop="toDate = undefined"
+                >
+                  <X />
+                </span>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent class="w-auto p-0" align="start">
+              <Calendar
+                v-model="toDate"
+                :default-placeholder="defaultPlaceholder"
+                layout="month-and-year"
+                initial-focus
+                @update:model-value="close"
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        <Button variant="outline"> Filter </Button>
+      </div>
+    </div>
+
+    <div class="relative">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead class="w-15">
+              <div class="flex items-center gap-1">
+                <Checkbox id="checkAll" />
+                <Label for="checkAll">All</Label>
+              </div>
+            </TableHead>
+            <TableHead class="min-w-15 w-15">Action</TableHead>
+            <TableHead class="min-w-35">Name</TableHead>
+            <TableHead class="min-w-50">Description</TableHead>
+            <TableHead class="min-w-20">Hourly Rate</TableHead>
+            <TableHead class="min-w-20">Overnight Rate</TableHead>
+            <TableHead class="min-w-20">Weekend Rate</TableHead>
+            <TableHead class="min-w-30">Effective From</TableHead>
+            <TableHead class="min-w-30">Effective To</TableHead>
+            <TableHead class="min-w-25">Status</TableHead>
+            <TableHead class="w-35">Created at</TableHead>
+            <TableHead class="w-35">Updated at</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TransitionGroup tag="tbody" name="row">
+          <TableRow
+            v-for="{
+              id,
+              name,
+              description,
+              hourlyRate,
+              overnightRate,
+              weekendRate,
+              effectiveFrom,
+              effectiveTo,
+              status,
+              created_at,
+              updated_at,
+            } in paginatedRateSheets"
+            :key="id"
+            :class="deletingIds[id] ? 'bg-red-100 dark:bg-red-950/30' : ''"
+          >
+            <TableCell class="font-medium">
+              <Checkbox />
+            </TableCell>
+            <TableCell>
+              <RateSheetAction :id="id" />
+            </TableCell>
+            <TableCell>{{ name }}</TableCell>
+            <TableCell>{{ description }}</TableCell>
+            <TableCell>{{ hourlyRate }}</TableCell>
+            <TableCell>{{ overnightRate }}</TableCell>
+            <TableCell>{{ weekendRate }}</TableCell>
+            <TableCell>{{ effectiveFrom }}</TableCell>
+            <TableCell>{{ effectiveTo }}</TableCell>
+            <TableCell>
+              <span v-html="getStatus(status)"></span>
+            </TableCell>
+            <TableCell>
+              <span v-html="ukFormat(created_at)"></span>
+            </TableCell>
+            <TableCell> <span v-html="ukFormat(updated_at)"></span> </TableCell>
+          </TableRow>
+        </TransitionGroup>
+      </Table>
+
+      <ActivityPagination
+        v-model:page="page"
+        :total="totalRateSheets"
+        :items-per-page="itemsPerPage"
+      />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.row-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.row-leave-to {
+  opacity: 0;
+}
+</style>
